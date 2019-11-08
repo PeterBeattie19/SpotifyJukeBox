2b7fd8f2decf90d2135d93c38382c155eb8d0cbb
Peter Beattie <peterbeattie19@gmail.com>
2019-11-08 21:45:06 +0000

    added docstrings, created delete func, added db file write every x insertions, added destructor

--- json_database.py
+++ json_database.py
@@ -2,9 +2,16 @@ import json
 
 
 class JsonDatabase:
-    def __init__(self, db_filename):
+    def __init__(self, db_filename, write_freq=5):
+        """
+        Constructor
+        :param db_filename: database file location and name
+        :param write_freq: how often to re-write the database file with updated entries
+        """
         self._db_filename = db_filename
         self._data = self._read(self._db_filename)
+        self._write_freq = write_freq
+        self._num_writes = 0
 
     @property
     def db_filename(self):
@@ -19,25 +26,64 @@ class JsonDatabase:
         return len(self._data)
 
     def select(self, key, value):
+        """
+        Select all entries where entry[key] == value
+        :param key: Hashable object
+        :param value: Object
+        :return: list
+        """
         return list(filter(lambda x: key in x and x[key] == value, self._data))
 
     def insert(self, obj):
-        # TODO Call Write every x inserts and when object is destructed __del__
+        """
+        Inserts entry into Database
+        :param obj: object to insert (dictionary)
+        :return: None
+        """
         try:
             assert isinstance(obj, dict)
         except AssertionError:
             raise TypeError
         else:
-            # TODO Write function to return File object for json database file
+            self._num_writes += 1
             self._data.append(obj)
-            with open(self._db_filename, 'w') as w_file:
-                json.dump(self._data, w_file)
+            if self._num_writes % self._write_freq == 0:
+                self._update_db_file()
 
-    def delete(self):
-        pass
+    def _update_db_file(self):
+        """
+        Write contents of self._data to database file
+        :return:
+        """
+        with open(self._db_filename, 'w') as w_file:
+            json.dump(self._data, w_file)
+
+    def delete(self, key, value):
+        """
+        Delete all entries where entry[key] == value
+        :param key: Hashable Object
+        :param value: Object
+        :return: None
+        """
+        res = []
+        for dict_ in self._data:
+            if key in dict_ and dict_[key] == value:
+                continue
+            res.append(dict_)
+        self._data = res
+        self._update_db_file()
 
     @staticmethod
     def _read(filename):
+        """
+        Read json file
+        :param filename: JSON file location and name
+        :return: list representing the JSON data
+        """
         with open(filename, 'r') as f:
             json_data = json.load(f)
         return json_data
+
+    def __del__(self):
+        """Write self._data contents to database file upon object destruction"""
+        self._update_db_file()
